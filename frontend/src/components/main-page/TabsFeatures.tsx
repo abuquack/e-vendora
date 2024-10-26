5a39791aa2b5157df900556829f8291e159330e2
Bakr A. Ibrahim <bakr14276@gmail.com>
2024-10-26 08:17:54 +0300

    feat(landing): Landing page is done

--- frontend/src/components/main-page/TabsFeatures.tsx
+++ frontend/src/components/main-page/TabsFeatures.tsx
@@ -30,8 +30,8 @@ function TabsFeatures() {
     },
   ]
   return (
-    <div className="flex flex-col">
-      <h1 className="font-bold text-2xl my-5 text-right">خطط وتعاون مع فريقك على متجرك مهما كبر حجمه</h1>
+    <div className="flex flex-col lg:w-1/2 lg:justify-center lg:items-center">
+      <h1 className="font-bold text-2xl my-5 w-full text-right">خطط وتعاون مع فريقك على متجرك مهما كبر حجمه</h1>
       <div>
         <ul className="flex flex-wrap gap-2">
           {
